43f4d95db7068bbfa3af5023851258f455b0386f
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-12-16 00:12:32 +0530

    Update Order.java
    Finished Order.java class

--- Order.java
+++ Order.java
@@ -1,2 +1,36 @@
+import java.util.ArrayList;
+
 public class Order {
+    private int orderId;
+    private ArrayList<FoodItem> items;
+    private double totalPrice;
+
+    public Order(int orderId) {
+        this.orderId = orderId;
+        this.items = new ArrayList<>();
+        this.totalPrice = 0.00;
+    }
+
+    public void addItem(FoodItem item) {
+        items.add(item);
+        totalPrice += item.getPrice();
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder orderDetails = new StringBuilder("Order ID: " + orderId +
+                "\nItems:\n");
+        for (FoodItem item : items) {
+            orderDetails.append("- ").append(item.getName())
+                    .append(": Rs.").append(String.format("%.2f", item.getPrice()))
+                    .append("\n");
+        }
+        orderDetails.append("Total: Rs.").append(String.format("%.2f", totalPrice));
+
+        return orderDetails.toString();
+    }
 }
