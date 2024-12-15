9529dbbb768aafc23df9f79930bd3390b974f5f4
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-12-15 22:34:45 +0530

    Update MenuManager.java
    add methods to add food and display menu

--- MenuManager.java
+++ MenuManager.java
@@ -1,2 +1,31 @@
+import java.util.ArrayList;
+
 public class MenuManager {
+    private ArrayList<FoodItem> menu;
+
+    public MenuManager() {
+        this.menu = new ArrayList<>();
+    }
+
+    // method to add a food item
+    public void addFoodItem(FoodItem item) {
+        menu.add(item);
+    }
+
+    //method to display menu
+    public void displayMenu() {
+        if(menu.isEmpty()) {
+            System.out.println("The menu is currently empty.");
+        }
+        else {
+            System.out.println("\nMenu Items:");
+
+            //for-each loop to read all objects in menu array
+            for (FoodItem item : menu) {
+                System.out.println(item);
+            }
+        }
+    }
+
+    
 }
