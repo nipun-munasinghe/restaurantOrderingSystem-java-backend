ecd02509c2b63fbebf3fbbba8e5ac46f2f1d369d
nipun-munasinghe <nipunlakmal884@gmail.com>
2024-12-15 21:54:17 +0530

    Update Main class with System's menu

--- Main.java
+++ Main.java
@@ -1,6 +1,27 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+        MenuManager menuManager = new MenuManager();
+        OrderManager orderManager = new OrderManager();
+        Scanner scanner = new Scanner(System.in);
+        boolean run = true;
 
+        //predefined menu items
+
+        System.out.println("Welcome to the Restaurant Ordering System!");
+
+        while (run) {
+            System.out.print("Menu:\n" +
+                    "1. View Menu\n" +
+                    "2. Place an order\n" +
+                    "3. View current orders\n" +
+                    "4. Update menu (Admin Only)\n" +
+                    "5. Exit\n" +
+                    "Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+        }
     }
 }
